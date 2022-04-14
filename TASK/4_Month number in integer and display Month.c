3ce8295d1fd198ea542101e7511f2b0c7ee6bcb9
Garfield_akash <62658494+akashraj21@users.noreply.github.com>
2022-04-14 10:32:08 +0530

    Create 4_Month number in integer and display Month.c

---
+++ TASK/4_Month number in integer and display Month.c
@@ -0,0 +1,39 @@
+/*Write a program in C to read any Month number in integer and display Month name in. the word.(using switch case)*/
+
+#include<stdio.h>
+void FindMonth(int);
+int main(){
+    int month;
+    printf("Enter Month Number\n");
+    scanf("%d",&month);
+    FindMonth(month);
+    return 0;
+}
+void FindMonth(int month){
+    switch(month){
+        case 1:printf("JAN");
+        break;
+        case 2:printf("FEB");
+        break;
+        case 3:printf("MAR");
+        break;
+        case 4:printf("APR");
+        break;
+        case 5:printf("MAY");
+        break;
+        case 6:printf("JUN");
+        break;
+        case 7:printf("JUL");
+        break;
+        case 8:printf("AUG");
+        break;
+        case 9:printf("SEP");
+        break;
+        case 10:printf("OCT");
+        break;
+        case 11:printf("NOV");
+        break;
+        case 12:printf("DEC");
+        break;
+    }
+}
